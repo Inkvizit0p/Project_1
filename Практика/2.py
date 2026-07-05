5c0ed249df3c72ca6c5b84b2d7072cacaf3face8
Inkvizit0p <inkvizito0p@gmail.com>
2026-07-06 01:09:49 +0300

    Add files via upload

--- Практика/2.py
+++ Практика/2.py
@@ -6,14 +6,11 @@ question = [i for i in json.load(open("eval_questions.json", encoding="utf-8")) 
 model = SentenceTransformer("sentence-transformers/paraphrase-multilingual-mpnet-base-v2")
 embedding_c = model.encode([i["description"] for i in corpus])
 embedding_q = model.encode([i["query"] for i in question])
-precision = ''
+precision = 0
 for i in range(len(question)):
     q = util.cos_sim(embedding_q[i], embedding_c)[0]
     top = sorted(q, reverse=True)[:3]
     f = q[int(question[i]["correct_chunk_id"][5:]) - 1]
-    print(f, top)
     if f in top:
-        precision += '1'
-    else:
-        precision += '0'
-print(precision, sum(int(i) for i in list(precision)))
+        precision += 1
+print(precision)
